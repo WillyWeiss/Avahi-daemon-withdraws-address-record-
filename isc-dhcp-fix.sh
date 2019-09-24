c44d647f16dc2241ec77d2ea3dd077c94de13627
Willy Weiss <weisswilly1985@gmail.com>
2019-09-24 20:36:09 +0100

    Update isc-dhcp-fix.sh

--- isc-dhcp-fix.sh
+++ isc-dhcp-fix.sh
@@ -1,43 +1,34 @@
 #!/bin/bash
 
-Card0(){
+Card0()
+{
 ifconfig eth0 | grep -Po '(?<=inet )[\d.]+' &> /dev/null
          if [ $? == 0 ];
             then
                 sleep 1
-                #Card1
             else
                  sudo dhclient eth0
         fi
 
 }
 
-#Card1(){
+#Card1()
+#{
 #ifconfig eth1/wlan0 | grep -Po '(?<=inet )[\d.]+' &> /dev/null
 #         if [ $? == 0 ];
 #            then
-#                Cardn
+#                sleep 1
 #            else
 #                sudo dhclient eth1/wlan0
-#                Cardn
+#               
 #fi
 #}
-#.........
-#.........
-#Cardn(){
-#ifconfig cardn | grep -Po '(?<=inet )[\d.]+' &> /dev/null
-#         if [ $? == 0 ];
-#            then
-#                Card0
-#           else
-#                sudo dhclient eth0
-#fi
-#}
+
+
 
 while true
 do
 Card0
 #Card1
-#....
-#Cardn
+#AddMoreCards
 done
